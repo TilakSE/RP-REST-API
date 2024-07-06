85fb3cadc02a2078cbf182c34e61a6c2f835f8c2
TilakSE <122595666+TilakSE@users.noreply.github.com>
2024-07-06 11:58:06 +0530

    Add files via upload

---
+++ static/js/request.js
@@ -0,0 +1,30 @@
+// static/js/request.js
+// Ajax Function : getData, sendForm
+
+export function getData(endpoint, callback) {
+  const request = new XMLHttpRequest();
+  request.onreadystatechange = () => {
+//arrow function of java script
+    if (request.readyState === 4) {
+// 4 indicates DONE state
+      callback(request.response);
+    }
+  };
+  request.open("GET", endpoint);
+  request.send();
+}
+
+export function sendForm(form, action, endpoint, callback) {
+  const formData = new FormData(form);
+  const dataJSON = JSON.stringify(Object.fromEntries(formData));
+
+  const request = new XMLHttpRequest();
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      callback(request.response, form);
+    }
+  };
+  request.open(action, endpoint);
+  request.setRequestHeader("Content-Type", "application/json");
+  request.send(dataJSON);
+}
